41b4285632746f99bb2d0b48e2c35fa88440e0b5
james <jameschambers732@gmail.com>
2024-12-07 15:19:39 +0100

    wikisum

--- myApp/src/App.jsx
+++ myApp/src/App.jsx
@@ -1,53 +1,18 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-import * as cheerio from 'cheerio'
+import { useState } from 'react'
+import {} from 'framer-motion'; 
+import anime from 'animejs'
 import './App.css'
-import $ from 'jquery'
 
-function AddForm(){
-  useEffect(() => {
-    const form = document.getElementById("form"); 
-    const input = document.getElementById("article"); 
-
-    form.addEventListener("submit", (e) => {
-      e.preventDefault()
-      if(input.value){
-        const link = "http://localhost:5173/api?text=" + input.value + "";
-        axios.get(link, {
-          url: link,
-          method: "get", 
-          responseType: "document",
-        }).then((value) => {
-          const $ = cheerio.load(value["data"]);
-          console.log(value["data"])
-
-          const words = $("h1").text();
-          console.log(words)
-
-          let x = document.createElement("h1");
-          x.classList.add("items");
-          x.innerText = value["data"]
-          document.getElementById("summary").appendChild(x);
-
-        })
-        input.value = ""
-      }
-    })
-  })
+function AddWiki(){
   return(
-    <form action="" method='get' id='form' className="w-[45em] h-[75%] m-auto p-[0] relative flex flex-col align-middle justify-center text-center ">
-      <div className="w-[100%] h-[80%] m-auto p-[0] relative flex flex-col align-middle justify-centere text-center bg-slate-700 " id='summary'></div>
-      <div className="w-[100%] h-[20%] m-auto p-[0] relative flex flex-row align-middle justify-centere text-center bg-transparent ">
-        <input type="text" id="article" placeholder="enter the wikipedia article" className="w-[75%] h-[100%] m-auto p-[0] relative flex flex-row align-middle justify-center text-center bg-slate-600 text-white text-2xl border-transparent  " />
-        <input type="submit" name="text" id="submit" value="summarize" className="w-[25%] h-[100%] m-auto p-[0] relative flex flex-col align-middle justify-center text-center bg-slate-900 text-white text-2xl border-transparent "  />
-      </div>
-    </form>
+    <div className="relative w-[25em] h-[50em] m-auto p-[0] flex flex-col align-middle justify-center text-center ">
+      <motion.div className="w-[100%] min-h-[] m-auto p-[0] relative "></motion.div>
+    </div>
   )
 }
 export default function App(){
   return(
-    <div className="relative w-[100%] h-[100%] m-auto p-[0] bg-transparent flex flex-col align-middle justify-center text-center ">
-      <AddForm></AddForm>
+    <div className="relative w-[100%] h-[100%] m-auto p-[0] flex flex-col align-middle justify-center text-center ">
     </div>
   )
 }
--- myApp/vite.config.js
+++ myApp/vite.config.js
@@ -4,18 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    open: "/index.html",
-    proxy: {
-      "/api": {
-        target: "http://127.0.0.1:5001/wikisum-a8c7c/us-central1/obj",
-        changeOrigin: true
-      }
-    },
-    cors: {
-      origin: true, 
-      credentials: true,
-      methods: "GET"
-    }
-  }
 })
