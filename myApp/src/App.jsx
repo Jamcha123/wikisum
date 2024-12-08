7e4c38ef730d776e48fdf8d64201e99a25c17052
Jamcha123 <jameschambers732@gmail.com>
2024-12-08 17:20:15 +0100

    finished

--- myApp/src/App.jsx
+++ myApp/src/App.jsx
@@ -2,20 +2,42 @@ import { useEffect, useState } from 'react'
 import {motion} from 'framer-motion'; 
 import $ from 'jquery'
 import anime from 'animejs'
+import axios from 'axios'; 
+import * as cheerio from 'cheerio'
 import './App.css'
 
 function AddWiki(){
   const [active, setActive] = useState(true); 
   useEffect(() => {
     const forms = document.getElementById("form"); 
+    const input = document.getElementById("article");
+    const text = document.getElementById("text") 
     forms.addEventListener("submit", (e) => {
-      e.preventDefault(); 
+      e.preventDefault()
+      const arr = []
+      $("#text").empty()
       document.getElementById("arrow").classList.add("change"); 
+
+      const link = "https://obj-6ezwxbgcda-uc.a.run.app?text=" + input.value 
+      axios.get(link, {
+        url: link,
+        method: "get",
+        responseType: "document", 
+      }).then((value) => {
+        const $ = cheerio.load(new XMLSerializer().serializeToString(value["data"]))
+        let data = $("body").text()
+
+        let x = document.createElement("p")
+        x.innerText = data
+        text.appendChild(x)
+        data = ""
+      })
+      input.value = ""
     })
   })
   return(
-    <div className="relative w-[75%] h-[100vh] m-auto p-[0] flex flex-col align-middle justify-center text-center ">
-      <motion.div id="box" initial={{height: 10 + "vh"}} animate={{height: active? 80 + "vh" : 10 + "vh"}} transition={{type: "keyframes", duration: 2}} className="w-[85%] h-[10vh] m-0 p-[0] relative flex flex-col align-middle justify-center text-center bg-slate-800 ">
+    <div className="relative w-[85%] h-[100vh] m-auto p-[0] flex flex-col align-middle justify-center text-center ">
+      <motion.div id="box" initial={{height: 10 + "vh"}} animate={{height: active? 80 + "vh" : 10 + "vh"}} transition={{type: "keyframes", duration: 2}} className="w-[100%] h-[10vh] m-0 p-[0] relative flex flex-col align-middle justify-center text-center bg-slate-800 ">
         <div className="flex flex-row align-middle justify-center text-center w-[100%] h-[10vh] m-auto p-[0] ">
           <div className="w-[75%] h-[100%] mt-[0%] m-auto p-[0] relative flex flex-row align-middle justify-center text-center ">
             <div className="w-[fit-content] h-[100%] m-0 lg:ml-[10%] p-[0] flex flex-col align-middle justify-center text-center">
@@ -31,11 +53,11 @@ function AddWiki(){
             </motion.span>
           </div>
         </div>
-        <motion.div id="text" style={{display: active? "flex" : "none"}} initial={{scaleY: 0}} animate={{scaleY: active? 1 : 0}} transition={{type: "keyframes", duration: 2}} className="w-[100%] h-[80vh] m-auto p-[0] relative bg-transparent flex flex-col align-middle justify-center text-center bg-slate-600 ">
+        <motion.div id="text" initial={{scaleY: 0, translateY: 100 + "%"}} animate={{scaleY: active? 1 : 0, translateY: active? 0 : 100 + "%"}} transition={{type: "keyframes", duration: 2}} className="w-[100%] h-[80vh] m-auto p-[0] relative bg-transparent flex flex-col align-middle justify-center text-center bg-slate-600 ">
 
         </motion.div>
       </motion.div>
-      <motion.div className="w-[85%] min-h-[10vh] max-h-[fit-content] m-0 p-[0] relative flex flex-row align-middle justify-center text-center bg-slate-600 ">
+      <motion.div className="w-[100%] min-h-[10vh] max-h-[fit-content] m-0 p-[0] relative flex flex-row align-middle justify-center text-center bg-slate-600 ">
         <form action="/" method='get' id='form' className="w-[100%] h-[10vh] m-auto p-[0] relative flex flex-row align-middle justify-center text-center">
             <input type="text" placeholder="enter a wikipedia article" className="w-[75%] h-[100%] m-auto p-[0] relative text-center text-2xl text-white bg-transparent "  id="article" />
             <input type="submit" value="Submit" id='submit' className="w-[25%] h-[100%] cursor-pointer m-auto p-[0] relative text-center text-2xl text-white border-[1px] border-white" />
--- myApp/vite.config.js
+++ myApp/vite.config.js
@@ -3,5 +3,19 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], 
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://obj-288814584965.us-central1.run.app", 
+        changeOrigin: true,
+
+      } 
+    },
+    cors: {
+      origin: "https://obj-288814584965.us-central1.run.app", 
+      methods: "GET",
+      credentials: true
+    }
+  }
 })
